a98722a7dafb29694f090f1cd6c912b017611fe1
Jason Rhinelander <jason@imaginary.ca>
2021-10-21 21:03:19 -0300

    pyoxenmq overhaul
    - Wrap much more of the OxenMQ
    - Add extensive pydoc to everything
    - Switch setup.py to pybind's built-in tools

--- setup.py
+++ setup.py
@@ -1,74 +1,31 @@
-import os
-import re
-import sys
-import platform
-import subprocess
+from setuptools import setup
 
-from setuptools import setup, Extension
-from setuptools.command.build_ext import build_ext
-from distutils.version import LooseVersion
+# Available at setup time due to pyproject.toml
+from pybind11.setup_helpers import Pybind11Extension, build_ext
 
+__version__ = "1.0.0"
 
-class CMakeExtension(Extension):
-    def __init__(self, name, sourcedir=''):
-        Extension.__init__(self, name, sources=[])
-        self.sourcedir = os.path.abspath(sourcedir)
+# Note:
+#   Sort input source files if you glob sources to ensure bit-for-bit
+#   reproducible builds (https://github.com/pybind/python_example/pull/53)
 
-
-class CMakeBuild(build_ext):
-    def run(self):
-        try:
-            out = subprocess.check_output(['cmake', '--version'])
-        except OSError:
-            raise RuntimeError("CMake must be installed to build the following extensions: " +
-                               ", ".join(e.name for e in self.extensions))
-
-        if platform.system() == "Windows":
-            cmake_version = LooseVersion(re.search(r'version\s*([\d.]+)', out.decode()).group(1))
-            if cmake_version < '3.1.0':
-                raise RuntimeError("CMake >= 3.1.0 is required on Windows")
-
-        for ext in self.extensions:
-            self.build_extension(ext)
-
-    def build_extension(self, ext):
-        extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
-        # required for auto-detection of auxiliary "native" libs
-        if not extdir.endswith(os.path.sep):
-            extdir += os.path.sep
-
-        cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
-                      '-DPYTHON_EXECUTABLE=' + sys.executable]
-
-        cfg = 'Debug' if self.debug else 'Release'
-        build_args = ['--config', cfg]
-
-        if platform.system() == "Windows":
-            cmake_args += ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(cfg.upper(), extdir)]
-            if sys.maxsize > 2**32:
-                cmake_args += ['-A', 'x64']
-            build_args += ['--', '/m']
-        else:
-            cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
-            build_args += ['--', '-j2']
-
-        env = os.environ.copy()
-        env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(env.get('CXXFLAGS', ''),
-                                                              self.distribution.get_version())
-        if not os.path.exists(self.build_temp):
-            os.makedirs(self.build_temp)
-        subprocess.check_call(['cmake', ext.sourcedir] + cmake_args, cwd=self.build_temp, env=env)
-        subprocess.check_call(['cmake', '--build', '.'] + build_args, cwd=self.build_temp)
+ext_modules = [Pybind11Extension(
+        "oxenmq",
+        ["src/bencode.cpp", "src/module.cpp", "src/oxenmq.cpp"],
+        cxx_std=17,
+        libraries=["oxenmq"],
+        ),
+]
 
 setup(
-    name='pyoxenmq',
-    version='0.1.0',
-    author='Jeff Becker',
-    author_email='jeff@i2p.rocks',
-    description='pybind oxenmq bindings',
-    long_description='',
-    ext_modules=[CMakeExtension('pyoxenmq')],
-    packages=["lokinet.auth"],
-    cmdclass=dict(build_ext=CMakeBuild),
+    name="oxenmq",
+    version=__version__,
+    author="Jason Rhinelander",
+    author_email="jason@oxen.io",
+    url="https://github.com/oxen-io/oxen-mq",
+    description="Python wrapper for oxen-mq message passing library",
+    long_description="",
+    ext_modules=ext_modules,
+    cmdclass={"build_ext": build_ext},
     zip_safe=False,
 )
